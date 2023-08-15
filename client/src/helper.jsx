2d29eb288d26c8c781b3af4ffe8a615dcddd1692
Madiha Umair <madiha.altaf@yahoo.com>
2023-08-15 00:31:00 -0400

    base-url

--- client/src/helper.jsx
+++ client/src/helper.jsx
@@ -1,11 +1,6 @@
-
-
 //const BASE_URL="https://playpal-green.vercel.app";
-//const BASE_URL = 'https://playpalbackend.onrender.com';
-
-const BASE_URL="https://playpal.onrender.com";
-
 //const BASE_URL = 'https://playpalbackend-production.up.railway.app';
 //const BASE_URL="http://localhost:8000";
 
+const BASE_URL="https://playpal.onrender.com";
 export { BASE_URL };
